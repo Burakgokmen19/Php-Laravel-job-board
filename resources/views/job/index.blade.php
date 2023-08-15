9fa9e01424300149de4b584ee73b25bb53242fbb
Burakgokmen19 <burakgokmn19@gmail.com>
2023-08-15 10:49:56 +0300

    first commit

---
+++ resources/views/job/index.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+    @foreach ($jobs as $job)
+       <x-card class="mb-4">
+    <div class="flex justify-between">
+    <h2 class="text-lg font-medium">{{$job->title}}</h2>
+    <div class="text-slate-500">
+        {{number_format($job->salary)}}₺
+    </div>
+   </div>
+
+   <div class="mb-4  flex  items-center justify-between text-sm text-slate-500">
+    <div class="flex space-x-4">
+    <div>Company Name</div>
+    <div>{{$job->location}}</div>
+   </div>
+    <div class="rounded-md border px-2 py-1">{{Str::ucfirst( $job->experience) }}</div>
+    <div class="rounded-md border px-2 py-1">{{$job->category}}</div>
+   </div>
+    </div>
+
+   <p class="text-sm text-slate-500">
+       {!! nl2br(e($job->description)) !!}
+   </p>
+    </x-card>
+    @endforeach
+</x-layout>
